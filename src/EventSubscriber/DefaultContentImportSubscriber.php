fe30cc4e98b32255da912fcc81dddd8e77a2a3b7
ekes <ekes@iskra.net>
2022-07-15 16:06:12 +0200

    Actively assign the microsites default content id to the default content.
    Rather than relying on it being /node/1, which will be removed when
    fixing potential errors in localgov_microsites_group.

---
+++ src/EventSubscriber/DefaultContentImportSubscriber.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Drupal\localgov_microsites\EventSubscriber;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\default_content\Event\DefaultContentEvents;
+use Drupal\default_content\Event\ImportEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * LocalGov Drupal Microsites event subscriber.
+ */
+class DefaultContentImportSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Config Factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Constructs event subscriber.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory service.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * Kernel request event handler.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
+   *   Response event.
+   */
+  public function onContentImport(ImportEvent $event) {
+    if ($event->getModule() == 'localgov_microsites') {
+      foreach ($event->getImportedEntities() as $entity) {
+        if ($entity->uuid() == 'f7e60359-efb1-4cd1-a4c8-b120fb4f5c41') {
+          $config = $this->configFactory->getEditable('localgov_microsites_group.settings');
+          $config->set('default_group_node', $entity->id());
+          $config->save();
+          break;
+        }
+      }
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      DefaultContentEvents::IMPORT => ['onContentImport'],
+    ];
+  }
+
+}
